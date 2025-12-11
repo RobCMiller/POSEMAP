9de7e7a711f92c30fbfacab6d5ede656ffdc15ec
RobCMiller <RobMiller095@Gmail.com>
2025-12-11 10:31:42 -0500

    Try different volume transpose [y,x,z] with R.T Euler angles and inverse transform

--- particle_mapper.py
+++ particle_mapper.py
@@ -553,9 +553,9 @@ def simulate_em_projection_from_pdb_eman2(pdb_data: Dict, euler_angles: np.ndarr
         zoom_factor_w = w / proj_w
         proj_array = zoom(proj_array, (zoom_factor_h, zoom_factor_w), order=1)
     
-    # Try both flips with R.T + inverse approach
-    proj_array = np.flipud(proj_array)  # Flip vertically
-    proj_array = np.fliplr(proj_array)  # Flip horizontally
+    # Try no flips with different volume orientation
+    # proj_array = np.flipud(proj_array)  # Flip vertically
+    # proj_array = np.fliplr(proj_array)  # Flip horizontally
     
     return proj_array
 
