ee8e4e2f1fce7e83c088279bc81ab4d1a0e1e29f
RobCMiller <RobMiller095@Gmail.com>
2025-12-11 10:32:17 -0500

    Try R.T Euler angles with inverse transform, transpose and horizontal flip

--- particle_mapper.py
+++ particle_mapper.py
@@ -553,9 +553,9 @@ def simulate_em_projection_from_pdb_eman2(pdb_data: Dict, euler_angles: np.ndarr
         zoom_factor_w = w / proj_w
         proj_array = zoom(proj_array, (zoom_factor_h, zoom_factor_w), order=1)
     
-    # Try transpose + vertical flip with R.T + inverse approach
+    # Try transpose + horizontal flip with R.T + inverse approach
     proj_array = proj_array.T  # Transpose
-    proj_array = np.flipud(proj_array)  # Flip vertically
+    proj_array = np.fliplr(proj_array)  # Flip horizontally
     
     return proj_array
 
