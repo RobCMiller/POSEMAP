db224bc5f3465fae7db4c24666a5b68814247d2f
RobCMiller <RobMiller095@Gmail.com>
2025-12-11 10:29:56 -0500

    Try transpose + vertical flip with corrected Euler angles and inverse

--- particle_mapper.py
+++ particle_mapper.py
@@ -551,8 +551,9 @@ def simulate_em_projection_from_pdb_eman2(pdb_data: Dict, euler_angles: np.ndarr
         zoom_factor_w = w / proj_w
         proj_array = zoom(proj_array, (zoom_factor_h, zoom_factor_w), order=1)
     
-    # Try transposing the projection array (swap axes)
+    # Try transpose + vertical flip
     proj_array = proj_array.T
+    proj_array = np.flipud(proj_array)  # Flip vertically
     
     return proj_array
 
